dec2e2efef59371da1b07d136ff95bedbbe76c0d
Ojerinde Joel <joelojerinde@gmail.com>
2024-04-06 07:35:30 -0600

    fix: fix signup error

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,7 +55,7 @@ const sendVerificationEmail = async (user, req, res, next) => {
       message: `A verification mail has been sent to ${user.email}`,
     });
   } catch (error) {
-    console.log("Email Sent");
+    console.log("Email Sent failed");
     user.emailVerificationToken = undefined;
     user.emailVerificationTokenExpiresIn = undefined;
     await user.save({ validateBeforeSave: false });
