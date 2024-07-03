ee5dac8921d38684894b81f49196feba3d34dec3
Ojerinde Joel <joelojerinde@gmail.com>
2024-07-03 20:11:13 +0100

    feat: add reports logic

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,6 +1,7 @@
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const { Lecturer, Course, Student, Attendance } = require("../models/appModel");
+const LevelAdviserUsers = require("../models/levelAdviserUserModel");
 
 exports.createLecturer = catchAsync(async (req, res, next) => {
   console.log("Creating Lecturer with", req.body);
