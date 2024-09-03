bd9798daf56834b222d83f5a6f966ebb88ade64f
Ojerinde <joelojerinde@gmail.com>
2024-09-03 11:28:04 +0100

    feat: update esp32 Detal logic

--- handlers/esp32DetailsHandler.js
+++ handlers/esp32DetailsHandler.js
@@ -2,14 +2,10 @@ const catchAsync = require("../utils/catchAsync");
 
 // Endpoint for fetching Esp32 details with Websocket.
 exports.esp32DetailsWithWebsocket = catchAsync(async (ws, clients, payload) => {
-  console.log(
-    "Starting to fetch Esp32 details with websocket",
-    payload,
-    clients
-  );
+  console.log("Starting to fetch Esp32 details with websocket", payload);
   const { deviceData } = payload;
   return clients.forEach((client) => {
-    // if (clients.clientType !== deviceData.deviceLocation) return;
+    if (clients.clientType !== deviceData.deviceLocation) return;
     client.send(
       JSON.stringify({
         event: "esp32_data_request",
@@ -25,7 +21,7 @@ exports.esp32DetailsWithWebsocket = catchAsync(async (ws, clients, payload) => {
 exports.esp32DetailsFeedback = catchAsync(async (ws, clients, payload) => {
   console.log("Received feedback from ESP32 device:", payload);
 
-  const { deviceData } = payload;
+  const { deviceData } = payload?.data;
 
   if (payload.error) {
     return clients.forEach((client) => {
