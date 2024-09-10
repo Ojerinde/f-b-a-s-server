53497e9937cb7639ac20944a907f46ae74aa66b4
Ojerinde <joelojerinde@gmail.com>
2024-09-10 15:57:53 +0100

    feat: revert logic to check for available id

--- handlers/enrollHandler.js
+++ handlers/enrollHandler.js
@@ -80,16 +80,11 @@ exports.enrollStudentWithWebsocket = catchAsync(
         );
       });
     } else {
-      // Fetch all existing idOnSensor values
-      const existingIds = await Student.find({
-        idOnSensor: { $ne: null },
-      }).select("idOnSensor");
-      const idSet = new Set(existingIds.map((student) => student.idOnSensor));
-
       // Find an available proposedId between 1 and 299
       let proposedId;
       for (let i = 1; i <= 299; i++) {
-        if (!idSet.has(i)) {
+        const idExists = await Student.findOne({ idOnSensor: i });
+        if (!idExists) {
           proposedId = i;
           break;
         }
