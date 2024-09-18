6ceb9701bd91f192a8a1026ab0062c8f2c996d19
Ojerinde <joelojerinde@gmail.com>
2024-09-18 02:14:37 +0100

    feat: update device location format

--- wss_server.js
+++ wss_server.js
@@ -79,21 +79,21 @@ function initWebSocketServer() {
             await DevicesConnected.create({
               deviceLocation: deviceLocation,
             });
-            // Start tracking the device immediately upon identification
-            if (!connectedDevices[deviceLocation]) {
-              connectedDevices[deviceLocation] = {
-                timeout: setTimeout(async () => {
-                  console.log(
-                    `Device ${deviceLocation} has not sent a ping in 10 seconds. Removing...`
-                  );
-                  await DevicesConnected.findOneAndDelete({ deviceLocation });
-                  delete connectedDevices[deviceLocation];
-                }, 10000), // 10-second timeout
-              };
-              console.log(
-                `Device ${deviceLocation} added and is being tracked.`
-              );
-            }
+            // // Start tracking the device immediately upon identification
+            // if (!connectedDevices[deviceLocation]) {
+            //   connectedDevices[deviceLocation] = {
+            //     timeout: setTimeout(async () => {
+            //       console.log(
+            //         `Device ${deviceLocation} has not sent a ping in 10 seconds. Removing...`
+            //       );
+            //       await DevicesConnected.findOneAndDelete({ deviceLocation });
+            //       delete connectedDevices[deviceLocation];
+            //     }, 10000), // 10-second timeout
+            //   };
+            //   console.log(
+            //     `Device ${deviceLocation} added and is being tracked.`
+            //   );
+            // }
           }
           break;
         case "enroll":
@@ -133,51 +133,51 @@ function initWebSocketServer() {
       }
     });
 
-    ws.on("ping", async (buffer) => {
-      const locationUtf8 = buffer.toString("utf8").toLowerCase();
-      console.log(
-        "Received ping from hardware",
-        buffer,
-        "location",
-        locationUtf8
-      );
-      console.log("Connected Devices", connectedDevices);
+    // ws.on("ping", async (buffer) => {
+    //   const locationUtf8 = buffer.toString("utf8").toLowerCase();
+    //   console.log(
+    //     "Received ping from hardware",
+    //     buffer,
+    //     "location",
+    //     locationUtf8
+    //   );
+    //   console.log("Connected Devices", connectedDevices);
 
-      if (connectedDevices[locationUtf8]) {
-        console.log(`Ping received from ${locationUtf8}, resetting timeout.`);
+    //   if (connectedDevices[locationUtf8]) {
+    //     console.log(`Ping received from ${locationUtf8}, resetting timeout.`);
 
-        console.log(
-          `Clearing previous timeout for ${locationUtf8}, Timeout ID:`,
-          connectedDevices[locationUtf8].timeout._idleTimeout
-        );
+    //     console.log(
+    //       `Clearing previous timeout for ${locationUtf8}, Timeout ID:`,
+    //       connectedDevices[locationUtf8].timeout._idleTimeout
+    //     );
 
-        clearTimeout(connectedDevices[locationUtf8].timeout);
+    //     clearTimeout(connectedDevices[locationUtf8].timeout);
 
-        connectedDevices[locationUtf8].timeout = setTimeout(async () => {
-          console.log(
-            `Device ${locationUtf8} has not sent a ping in 10 seconds. Removing...`
-          );
-          await DevicesConnected.findOneAndDelete({
-            deviceLocation: locationUtf8,
-          });
-          delete connectedDevices[locationUtf8];
-        }, 10000);
-      } else {
-        console.log(`Tracking new device ${locationUtf8} for pings.`);
-        connectedDevices[locationUtf8] = {
-          timeout: setTimeout(async () => {
-            console.log(
-              `Device ${locationUtf8} has not sent a ping in 10 seconds. Removing...`
-            );
-            await DevicesConnected.findOneAndDelete({
-              deviceLocation: locationUtf8,
-            });
-            delete connectedDevices[locationUtf8];
-          }, 10000),
-        };
-        console.log(`Device ${locationUtf8} added and is being tracked.`);
-      }
-    });
+    //     connectedDevices[locationUtf8].timeout = setTimeout(async () => {
+    //       console.log(
+    //         `Device ${locationUtf8} has not sent a ping in 10 seconds. Removing...`
+    //       );
+    //       await DevicesConnected.findOneAndDelete({
+    //         deviceLocation: locationUtf8,
+    //       });
+    //       delete connectedDevices[locationUtf8];
+    //     }, 10000);
+    //   } else {
+    //     console.log(`Tracking new device ${locationUtf8} for pings.`);
+    //     connectedDevices[locationUtf8] = {
+    //       timeout: setTimeout(async () => {
+    //         console.log(
+    //           `Device ${locationUtf8} has not sent a ping in 10 seconds. Removing...`
+    //         );
+    //         await DevicesConnected.findOneAndDelete({
+    //           deviceLocation: locationUtf8,
+    //         });
+    //         delete connectedDevices[locationUtf8];
+    //       }, 10000),
+    //     };
+    //     console.log(`Device ${locationUtf8} added and is being tracked.`);
+    //   }
+    // });
 
     ws.on("close", async () => {
       console.log(`${ws?.clientType} client is disconnected`);
